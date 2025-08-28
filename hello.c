89e48ea333b17cdfa21c1fe69338bede0514b0ec
TheAngryGhost <theangryghost@protonmail.com>
2025-08-28 10:14:24 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Casper Turek
+ *         cturek12@unl.edu
+ * Date: 2025/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World, I'm Casper Turek!\n");
+    printf("My major is Computer Science\n");
+
+    return 0;
+}
